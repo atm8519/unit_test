c5746187bd2cd98758846938f1a943f0000e157e
helloworld <abisoyetimothy@yahoo.com>
2025-03-09 06:45:30 +0100

    check

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-input_first_number = 22
+input_first_number = 52
 input_second_number = 28
 sum_of_the_number = input_first_number + input_second_number
 print(f"\n......................................\nthe sum of {input_first_number} + {input_second_number} = {sum_of_the_number}")
