821f0e7917cd515fdf480cde3c3158104f053bda
helloworld <abisoyetimothy@yahoo.com>
2025-03-09 04:58:17 +0100

    updated  add.py

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 input_first_number = float(input("\nenter first number > "))
-input_second_number = float(input("\nenter second number > "))
+input_second_number = float(input("\nenter second number >> "))
 sum_of_the_number = input_first_number + input_second_number
 print(f"\n......................................\nthe sum of {input_first_number} + {input_second_number} = {sum_of_the_number}")
 
